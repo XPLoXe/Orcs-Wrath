93536c310b28f691a9f7a8579c28f010256c5456
Alejandro Bocchicchio <aleb2800@gmail.com>
2022-09-27 13:31:42 -0300

    Scripts and Animator created

--- Orcs/Assets/Scripts/PlayerController.cs
+++ Orcs/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Animator animator;
+    private float horizontalInput;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("IsRunning");
+            transform.Translate(Vector3.forward * horizontalInput * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetTrigger("IsRunning");
+            transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+            transform.Translate(-Vector3.forward * horizontalInput * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetTrigger("IsRunning");
+            transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
+            transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetTrigger("IsRunning");
+            transform.Rotate(0.0f, -90.0f, 0.0f, Space.Self);
+            transform.Translate(-Vector3.right * horizontalInput * speed * Time.deltaTime);
+        }
+
+        transform.Rotate(0.0f, 0.0f, 0.0f, Space.Self);
     }
 }
